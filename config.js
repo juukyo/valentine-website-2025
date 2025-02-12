7e0cbedb01a1b2d86214045a6a906bdded61df7a
juukyo <araniadork@gmail.com>
2025-02-12 20:10:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        hearts: ['❤️', '🤍', '🩶', ' 💜', '🩷'],  // Heart emojis
+        bears: ['🐼', '🐻','🐨','🦝','🐧','🐷','🦌']                       // Cute bear emojis
     },
 
     // Questions and answers
